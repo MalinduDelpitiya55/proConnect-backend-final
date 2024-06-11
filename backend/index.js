b741a19b950e93f77be374fb547ade931608cedd
Malindu Delpitiya <mdelpitiya66@gmail.com>
2024-06-11 21:33:11 +0530

    ,,

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ const connect = async () => {
   }
 };
 
-app.use(cors());
+app.use(cors("https://fiverr-clone-backend.vercel.app/"));
 app.use(express.json());
 app.use(cookieParser());
 const router = express.Router();
